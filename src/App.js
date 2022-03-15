847dde35a63ed62108bc9cac448c50796cc5f641
ChrisPag <chrisp99@my.yorku.ca>
2022-03-15 18:24:17 -0400

    Created draggable images using react-draggable

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Home from './components/Home.js';
 import Moodboard from './components/Moodboard.js';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { useState } from 'react';
-import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
 
 function App() {
   const [likes, setLikes] = useState();
@@ -12,14 +10,12 @@ function App() {
   return (
     
     <Router>
-      <DndProvider backend={HTML5Backend}>
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Home updateLikes={likes =>setLikes(likes)} />}/>
           <Route path="/moodboard" element ={<Moodboard likes = {likes} />}/>
         </Routes>
       </div>
-      </DndProvider>
     </Router>
   );
 }
--- src/components/Moodboard.js
+++ src/components/Moodboard.js
@@ -1,32 +1,24 @@
 import '../css/Moodboard.css';
 import { useNavigate } from 'react-router-dom';
-import {useDrop} from 'react-dnd';
-import Photo from './Photo'
-import {useState} from 'react';
-import { customDragLayer } from './customDragLayer';
-
-
+import {useState, useCallback, useEffect} from 'react';
+import Draggable from 'react-draggable';
+//example
 function Moodboard(props) {
   const navigate = useNavigate();
-  const [canvas, setCanvas] = useState([]);
-  
-  /* useDrop accepts two arguments
-  ** {isOver} determines if an image is dropped
-  ** drop references where the images can be dropped and calls a function */
-  const [{isOver}, drop] = useDrop(({
-    accept: "image",
-    drop: (item) => addToCanvas(item.index),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-    }),
-  }))
+  const [canvasPhotos, setCanvasPhotos] = useState([]);
 
   const addToCanvas = (index) =>{
-    const canvasPhotos = props.likes.filter((photo)=> props.likes.url === photo.index);
-    setCanvas((canvas)=> [...canvas, canvasPhotos[index]]);
+    const movedPhotos = props.likes.filter((photo)=> props.likes.url === photo.index);
+    setCanvasPhotos((canvasPhotos)=> [...canvasPhotos, movedPhotos[index]]);
   }
 
+  useEffect(()=>{
+    console.log(canvasPhotos);
+    setCanvasPhotos(canvasPhotos);
+  },[canvasPhotos]);
+
   return (
+    
     <div className="Moodboard">
       <div className="moodHeader"> 
         <button id="back" className="btn" onClick={()=> navigate(-1)}>Back</button>
@@ -36,21 +28,29 @@ function Moodboard(props) {
             {props.likes && 
               (props.likes.map((likes, i)=>(
               <div className="moodboard-imgs" key={i}>
-                  <Photo url={likes.url} alt={likes.alt} index={i} />
+                  <img src={likes.url} alt={likes.alt} index={i} />
+                  <button onClick={()=>addToCanvas(i)}>Add</button>
               </div>
-            )))}
+              )))}
 
           {!props.likes &&
           <p>Could not load images</p>}
         </div> 
       </div>
-      <div className= "canvas" ref={drop}>
-        {canvas.map((photo, i)=>{
-          return <Photo url={photo.url} alt={photo.alt} key={i} />
+      <div className= "canvas">
+        {/* The canvas is where liked images (beside the sidebar) can be added to*/}
+
+          {canvasPhotos.map((photo, i)=>{
+          return (
+      
+          <Draggable 
+          bounds="parent">
+            <img src={photo.url} alt={photo.alt} index={i}  />
+          </Draggable>)
         })}
       </div>
     </div>
   );
-  }
+}
   
-  export default Moodboard;
+export default Moodboard;
